8ee7c05335b8814e6f0f0cbdbe163ff9ceb6d9a1
gchan82 <42255277+gchan82@users.noreply.github.com>
2018-12-28 17:54:49 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+
+document.getElementById("button").onclick = function(){
+
+  document.getElementById("confirm").innerHTML = "Order placed. Check your email for confirmation.";
+  document.getElementById("button").style.display = "none";
+}
